c9b9d5c3d4231169c64b469fcb18bc48d37deb86
Andrew Rock <Andrew Rock>
2017-05-01 13:54:46 -0500

    moved return object

--- js/clear.js
+++ js/clear.js
@@ -4,9 +4,9 @@ var Messenger = (function (object){
 	object.clearAll = function(array) {
 		array = [];
 		Messenger.populateMessagePage(array);
-		document.getElementsById("clear-button").setAttribute("disabled", true);
-		return object;
+		document.getElementsById("clear-btn").setAttribute("disabled", true);
 	}
+	return object;
 })(Messenger || {})
 
 
