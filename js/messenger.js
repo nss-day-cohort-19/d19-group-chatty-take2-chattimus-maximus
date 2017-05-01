7fd86ea9dd9a78a28082233ea9ae28854631a1f5
Jason Smith <jasonlsmith52@gmail.com>
2017-05-01 13:57:54 -0500

    correct a couple variable names

--- js/messenger.js
+++ js/messenger.js
@@ -14,10 +14,10 @@ var Messenger = (function (taco){
 
 	taco.populateMessagePage = function(array) {
 		messages = array;
-		document.getElementById("clear-button").setAttribute("disable", false);
+		document.getElementById("clear-btn").setAttribute("disable", false);
 		var messagesString = "";
 		for(var i; i < messages.length; i++) {
-			messageString += `<div class="message"><p>${message[i].text}</p><button id="msg${i}">Delete</button></div>`;
+			messageString += `<div class="message"><p>${messages[i].text}</p><button id="msg${i}">Delete</button></div>`;
 		}
 		document.getElementById("messagePage").innerHTML = messageString;
 	};
