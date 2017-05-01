a55eefcb1e603ad0d1e7ce1c6430e122ce14644a
Andrew Rock <Andrew Rock>
2017-05-01 13:28:22 -0500

    clear-all button function

---
+++ js/clear.js
@@ -0,0 +1,14 @@
+console.log("clear.js loaded");
+
+var Messenger = (function (object){
+	object.clearAll = function(array) {
+		array = [];
+		Messenger.populateMessagePage(array);
+		document.getElementsById("clear-button").setAttribute("disabled", true);
+	}
+})(Messenger || {})
+
+
+
+
+
